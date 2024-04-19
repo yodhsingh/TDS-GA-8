9b484e857f39100df90a9a6f1b6b108ae6b57ac1
prashanth-1012 <165567782+prashanth-1012@users.noreply.github.com>
2024-04-19 15:25:20 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,9 +3,9 @@ from streamlit import title, header, subheader, write, number_input
 title('Maximum of three numbers')
 header("Enter three numbers")
 subheader("in the boxes below")
-a = number_input()
-b = number_input()
-c = number_input()
+a = number_input("Number 1:")
+b = number_input("Number 2:")
+c = number_input("Number 3:")
 
 write("**Maximum** _value_ :")
 write(max(a,b,c))
